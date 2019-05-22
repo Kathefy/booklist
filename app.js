4a82c865255164e961042d8ebb35c3ba5ee2a8ff
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-05-22 22:47:47 +0200

    Adding book to the list

---
+++ app.js
@@ -0,0 +1,39 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('booklist');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+  list.appendChild(row);
+};
+
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+};
+// Event listeners
+document.getElementById('bookForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title, author, isbn);
+  const ui = new UI();
+
+  ui.addBookToList(book);
+  ui.clearFields();
+});
