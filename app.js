1a88a344a77568add6db1729ffc93ae4d90f9b3e
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-05-30 21:39:39 +0200

    Converted to classes

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
 // Book Constructor
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 // UI Constructor
-function UI() {}
-
-UI.prototype.addBookToList = function(book) {
-  const list = document.getElementById('booklist');
-  const row = document.createElement('tr');
-  row.innerHTML = `
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById('booklist');
+    const row = document.createElement('tr');
+    row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
     <td><a href="#" class="delete">X</a></td>
     `;
-  list.appendChild(row);
-};
-
-UI.prototype.deleteBook = function(target) {
-  if (target.className === 'delete') {
-    target.parentElement.parentElement.remove();
+    list.appendChild(row);
   }
-};
 
-UI.prototype.showAlert = function(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert ${className}`;
-  div.appendChild(document.createTextNode(message));
-  const container = document.querySelector('.container');
-  const form = document.querySelector('#bookForm');
-  container.insertBefore(div, form);
+  deleteBook(target) {
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
 
-  setTimeout(function() {
-    document.querySelector('.alert').remove();
-  }, 3000);
-};
+  showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#bookForm');
+    container.insertBefore(div, form);
 
-UI.prototype.clearFields = function() {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
-};
+    setTimeout(function() {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+}
+
 // Event listeners
 document.getElementById('bookForm').addEventListener('submit', function(e) {
   e.preventDefault();
